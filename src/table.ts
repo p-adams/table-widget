4d763bed5e5b398dab8646881c9f0bcf9bb9f71f
Paul Adams <ghusiyy@gmail.com>
2023-08-23 23:07:34 -0700

    remove counter file

--- src/table.ts
+++ src/table.ts
@@ -1,5 +1,6 @@
 export function setupTable(element: HTMLDivElement, tableData: TableData) {
-  const tableStyle = `display: grid; grid-template-columns: repeat(${tableData.columnNames.length}, 1fr)`;
+  const $tableData = getTableData(tableData);
+  const tableStyle = `display: grid; grid-template-columns: repeat(${$tableData.columnNames.length}, 1fr)`;
   element.innerHTML = `<div id="table">
         <div id="thead" class="row th" style="${tableStyle}">
         </div>
@@ -9,14 +10,19 @@ export function setupTable(element: HTMLDivElement, tableData: TableData) {
 
   const $thead = document.querySelector("#thead");
   const $tbody = document.querySelector("#tbody");
-  for (const [i, name] of tableData.columnNames.entries()) {
+  for (const [_, name] of $tableData.columnNames.entries()) {
     const $el = document.createElement("div");
     $el.innerHTML = `<div>${name}</div>`;
     $thead?.appendChild($el);
   }
-  for (const [i, row] of tableData.rows.entries()) {
+  for (const [_, row] of $tableData.rows.entries()) {
     const $el = document.createElement("div");
     $el.innerHTML = `<div>${row.data}</div>`;
     $tbody?.appendChild($el);
   }
+}
+
+export function getTableData(tableData: TableData) {
+  // TODO: return validated and normalized table data
+  return tableData;
 }

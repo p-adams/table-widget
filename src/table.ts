f493c0b7f77d6b330da92c7fbe756eee468ba8a7
Paul Adams <ghusiyy@gmail.com>
2023-08-25 00:51:41 -0700

    fix table rendering

--- src/table.ts
+++ src/table.ts
@@ -1,80 +1,47 @@
-export function setupTable(
-  element: HTMLDivElement,
-  tableData: ConsumerTableData
-) {
-  const $tableData = getTableData(tableData);
-  const tableStyle = `display: grid; grid-template-columns: repeat(${$tableData.columnNames.length}, 1fr)`;
-  element.innerHTML = `<div id="table">
-        <div id="thead" class="row th" style="${tableStyle}">
+export function setupTable(element: HTMLDivElement, tableData: TableData) {
+  const tableStyle = `display: grid; grid-template-columns: repeat(${tableData.columnNames.length}, 1fr)`;
+  element.innerHTML = `<div id="table-wrapper">
+        <div id="filters">
+          <input placeholder="Search..."/>
         </div>
-        <div id="tbody" class="row td" style="${tableStyle}"> 
+        <div id="table">
+        
         </div>
     </div>`;
 
-  const $thead = document.querySelector("#thead");
-  const $tbody = document.querySelector("#tbody");
-  for (const [_, name] of $tableData.columnNames.entries()) {
-    const $el = document.createElement("div");
-    $el.innerHTML = `<div>${name}</div>`;
-    $thead?.appendChild($el);
-  }
-  for (const [_, row] of $tableData.rows.entries()) {
-    const $el = document.createElement("div");
-    $el.innerHTML = `<div>${row.data}</div>`;
-    $tbody?.appendChild($el);
-  }
-}
+  const tableElement = document.getElementById("table");
 
-export function getTableData(tableData: ConsumerTableData): TableData | null {
-  try {
-    return validateAndNormalizeTable(tableData);
-  } catch (error) {
-    // TODO: create error type
-    return null;
-  }
-}
+  function renderTable(tableData: TableData) {
+    tableElement!.innerHTML = "";
+    const headerRow = document.createElement("div");
+    headerRow.classList.add("header-row");
+    headerRow.style.display = "grid";
+    headerRow.style.gridTemplateColumns = `repeat(${tableData.columnNames.length}, 1fr)`;
 
-function validateAndNormalizeTable(tableData: ConsumerTableData): TableData {
-  const schema = {
-    expectedColumns: tableData.columnNames,
-  };
-
-  // Validate column names
-  if (
-    !Array.isArray(tableData.columnNames) ||
-    tableData.columnNames.length !== schema.expectedColumns.length ||
-    !tableData.columnNames.every(
-      (col, index) => col === schema.expectedColumns[index]
-    )
-  ) {
-    throw new Error("Invalid column names");
-  }
-
-  // Normalize and validate rows
-  const normalizedRows: TableRow[] = [];
-  tableData.rows.forEach((row) => {
-    const normalizedRow: TableRow = {
-      id: row.id,
-      data: {},
-    };
-
-    schema.expectedColumns.forEach((col) => {
-      let cellValue = row.data[col];
-
-      // Handle missing cells or non-string values
-      if (typeof cellValue !== "string") {
-        cellValue = "";
-      }
-
-      // Normalize the cell value to be a string
-      normalizedRow.data[col] = cellValue;
+    tableData.columnNames.forEach((columnName) => {
+      const headerCell = document.createElement("div");
+      headerCell.classList.add("header");
+      headerCell.textContent = columnName;
+      headerRow.appendChild(headerCell);
     });
 
-    normalizedRows.push(normalizedRow);
-  });
+    tableElement!.appendChild(headerRow);
 
-  return {
-    columnNames: schema.expectedColumns,
-    rows: normalizedRows,
-  };
+    // Create and append rows
+    tableData.rows.forEach((rowData) => {
+      const row = document.createElement("div");
+      row.classList.add("row");
+      row.style.display = "grid";
+      row.style.gridTemplateColumns = `repeat(${tableData.columnNames.length}, 1fr)`;
+      tableData.columnNames.forEach((columnName) => {
+        const cell = document.createElement("div");
+        cell.textContent = rowData.data[columnName.toLowerCase()];
+        row.appendChild(cell);
+      });
+
+      tableElement!.appendChild(row);
+    });
+  }
+
+  renderTable(tableData);
 }
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,14 +1,4 @@
-interface ConsumerTableData {
-  columnNames: Array<string>;
-  rows: Array<{ id: number | string; data: string }>;
-}
-
-interface TableRow {
-  id: number;
-  data: Record<string, string>;
-}
-
 interface TableData {
-  columnNames: string[];
-  rows: TableRow[];
+  columnNames: Array<string>;
+  rows: Array<{ id: number | string; data: any }>;
 }
